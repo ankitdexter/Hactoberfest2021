606402739fa2c77e6e40d87c758b41f5776842cd
Ankit Kumar Singh <ankitkumar60323@gmail.com>
2021-10-03 14:36:48 +0530

    calendar.py => a file which prints calendar
    Hi please add my file

---
+++ Python/calendar.py
@@ -0,0 +1,7 @@
+import calendar
+
+year =int( input("Enter the year of the required calendar "))
+month = int( input("Enter the month of the required calendar "))
+
+#printing the calendar
+print(calendar.month(year,month))
